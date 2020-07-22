e4b8c0788d15d005353aec298dbe785b4798489c
FredrickBarrett <f.barrett12@gmail.com>
2020-07-22 05:35:51 -0400

    set up initial redux actions

---
+++ redux/actions.js
@@ -0,0 +1,94 @@
+// API Constants
+
+const BASE_URL = 'http://localhost:3000';
+const USERS_URL = BASE_URL + '/users';
+const PERSIST_URL = BASE_URL + '/auth';
+const LOGIN_URL = BASE_URL + '/login';
+const SPECIFIC_USER_URL = id => USERS_URL + '/' + id;
+
+// Redux Actions
+
+const setUserAction = userObj => ({
+    type: 'SET_USER',
+    payload: userObj
+  });
+  
+  const clearUserAction = () => ({
+    type: 'CLEAR_USER'
+  })
+
+  // Fetches
+
+  const newUserToDB = (userObj, url) => dispatch => {
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userObj)
+    }
+    fetch(url, config)
+      .then(r => r.json())
+      .then(data => {
+        dispatch(setUserAction(data))
+        localStorage.setItem('token', data.token)
+        localStorage.setItem('role', data.role)
+      })
+  }
+  
+  const deleteUserFromDB = userId => dispatch => {
+    const config = {
+      method: 'DELETE'
+    }
+    fetch(SPECIFIC_USER_URL(userId), config).then(r => {
+      dispatch(clearUserAction())
+      localStorage.clear()
+    })
+  }
+  
+  
+  const loginUserToDB = userCredentials => dispatch => {
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userCredentials)
+    }
+    fetch(LOGIN_URL, config)
+      .then(r => r.json())
+      .then(data => {
+        console.log(data)
+        dispatch(setUserAction(data))
+        localStorage.setItem('token', data.token)
+        localStorage.setItem('role', data.role)
+      })
+  }
+  
+  const persistUser = () => dispatch => {
+    const config = {
+      method: 'GET',
+      headers: {
+        Authorization: `bearer ` + localStorage.token
+      }
+    }
+    fetch(PERSIST_URL, config)
+      .then(r => r.json())
+      .then(userInstance => {    
+        dispatch(setUserAction(userInstance))
+        localStorage.setItem('role', userInstance.role)
+      })
+  }
+  
+  const logoutUser = () => dispatch => {
+    dispatch(clearUserAction());
+    localStorage.clear();
+  }
+  
+  export default {
+    newUserToDB,
+    deleteUserFromDB,
+    loginUserToDB,
+    persistUser,
+    logoutUser
+  }
